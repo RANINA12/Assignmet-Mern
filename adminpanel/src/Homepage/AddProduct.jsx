437e77e13e9098a9678b9322437837f136e345a6
Nikunj Bisani <nikunjbisani@gmail.com>
2025-11-18 17:56:50 +0530

    Prepare for backend fix

--- adminpanel/src/Homepage/AddProduct.jsx
+++ adminpanel/src/Homepage/AddProduct.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import './AddProduct.css'
+import './Addproduct.css'
 import { toast } from "react-toastify";
 import CropImage from "./CropImage"
 
